bd23009bdf2be649bbf7fe4181a8fb81177da337
Test <adrienlegeleu@gmail.com>
2024-09-24 15:52:10 +0200

    coffeur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Spotlight } from "@/components/acernity/Spotlight";
+import { Header } from "@/features/layout";
 
 export default function Home() {
   return (
-    <div className="h-full antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-full  antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <div className="h-screen">
-        <h1>Coiffeur</h1>
         <Spotlight
           className="-top-40 left-0 md:left-60 md:-top-20"
           fill="white"
         />
+        <Header />
+        <h1>Coiffeur</h1>
       </div>
     </div>
   );
