be366e911da4dfc4c011bcc869b09a4f13f0247c
Test <adrienlegeleu@gmail.com>
2024-09-27 10:16:36 +0200

    coiffeur sirte page about

--- src/features/home/About.tsx
+++ src/features/home/About.tsx
@@ -3,6 +3,7 @@ import { AnimatedTooltip } from "@/components/acernity/animated-tooltip";
 import { HeroHighlight, Highlight } from "@/components/acernity/hero-highlight";
 import React from "react";
 import { motion } from "framer-motion";
+import { MarqueeDemo } from "@/components/acernity/reviewCard";
 
 const people = [
   {
@@ -49,12 +50,25 @@ export function About() {
           ease: [0.4, 0.0, 0.2, 1],
         }}
       >
-        <Highlight className="text-black text-5xl  font-semibold tracking-wider text-center ">
+        <Highlight className="text-black text-5xl w-full  font-semibold tracking-wider text-center ">
           A propos de nous
         </Highlight>
       </motion.h1>
-      <div className="flex flex-row items-center justify-center mt-32 mb-10 w-full">
-        <AnimatedTooltip items={people} />
+      <div className="mt-32 grid grid-cols-2 ">
+        <div>
+          <div className="flex flex-row items-center justify-center  mb-10 w-full">
+            <AnimatedTooltip items={people} />
+          </div>
+          <p className="text-lg font-semibold tracking-wider leading-loose text-center px-20">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
+            asperiores reprehenderit alias, quod culpa veniam sint
+            necessitatibus magnam doloribus vero, libero a at et quas ea.
+            Deserunt temporibus saepe rerum at asperiores.
+          </p>
+        </div>
+        <div>
+          <MarqueeDemo />
+        </div>
       </div>
     </HeroHighlight>
   );
